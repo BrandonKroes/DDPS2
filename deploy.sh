2b10577c0a31362e5dbbcf765c9985ec9603bf6b
Brandon Kroes <14329518+BrandonKroes@users.noreply.github.com>
2022-12-08 00:15:23 +0100

    its working

--- deploy.sh
+++ deploy.sh
@@ -36,26 +36,26 @@ echo "worker is "$worker
 blender_path="/local/$USER/blender-3.3.1-linux-x64"
 repo="https://github.com/BrandonKroes/DDPS2.git"
 
-#rm -rf /var/scratch/$USER/blender-3.3.1-linux-x64
-#rm -r /home/$USER/blender; mkdir /home/$USER/blender
+rm -rf /var/scratch/$USER/blender-3.3.1-linux-x64
+rm -r /home/$USER/blender; mkdir /home/$USER/blender
 
-#wget -O /var/scratch/$USER/blender-3.3.1-linux-x64.tar.xz https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.3/blender-3.3.1-linux-x64.tar.xz
-#tar -xvf /var/scratch/$USER/blender-3.3.1-linux-x64.tar.xz -C /home/$USER/blender
+wget -O /var/scratch/$USER/blender-3.3.1-linux-x64.tar.xz https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.3/blender-3.3.1-linux-x64.tar.xz
+tar -xvf /var/scratch/$USER/blender-3.3.1-linux-x64.tar.xz -C /home/$USER/blender
 
 echo "starting the master!"
 # clone the project to master
 
 
 # removing possible residual data
-#ssh -T $master "rm -r /local/$USER/; mkdir /local/$USER/ && exit"
+ssh -T $master "rm -r /local/$USER/; mkdir /local/$USER/ && exit"
 
-#ssh -T $master "nohup git clone https://github.com/BrandonKroes/DDPS2.git /local/$USER/DDPS2"
-#ssh -T $master "truncate -s 0 /local/$USER/DDPS2/config/conf.yaml"
-#ssh -T $master 'echo $"master:" >> /local/$USER/DDPS2/config/conf.yaml'
-#ssh -T $master 'echo $" host:'$master'" >> /local/$USER/DDPS2/config/conf.yaml'
-#ssh -T $master 'echo $" port:'$master_port'" >> /local/$USER/DDPS2/config/conf.yaml'
+ssh -T $master "nohup git clone https://github.com/BrandonKroes/DDPS2.git /local/$USER/DDPS2"
+ssh -T $master "truncate -s 0 /local/$USER/DDPS2/config/conf.yaml"
+ssh -T $master 'echo $"master:" >> /local/$USER/DDPS2/config/conf.yaml'
+ssh -T $master 'echo $" host:'$master'" >> /local/$USER/DDPS2/config/conf.yaml'
+ssh -T $master 'echo $" port:'$master_port'" >> /local/$USER/DDPS2/config/conf.yaml'
 
-#ssh -T $master "nohup python3.6 /local/$USER/DDPS2/test/master-test.py > master.log &"
+ssh -T $master "nohup python3.6 /local/$USER/DDPS2/test/master-test.py > master.log &"
 
 
 for n in ${worker}; do
@@ -83,10 +83,10 @@ ssh -T $n 'echo $" host:'$master'" >> /local/$USER/DDPS2/config/conf.yaml'
 ssh -T $n 'echo $" port:'$master_port'" >> /local/$USER/DDPS2/config/conf.yaml'
 
 #echo "starting node $n as worker"
-#ssh -T $n "nohup ./local/$USER/DDPS2/test/worker-test.py > debug.log &"
+ssh -T $n "nohup ./local/$USER/DDPS2/test/worker-test.py > debug.log &"
 
 done
 
-#sleep 10 # launching all workers can take some time
+sleep 10 # launching all workers can take some time
 
 
