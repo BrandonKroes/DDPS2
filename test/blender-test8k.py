8f4b41819e5b4307c5327241832b20a7ac3834e6
Brandon Kroes <14329518+BrandonKroes@users.noreply.github.com>
2022-12-08 21:53:56 +0100

    added the new blender renders for testing

---
+++ test/blender-test8k.py
@@ -0,0 +1,20 @@
+import sys
+import os.path
+
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from common.packets import NewOperationPacket, JobType
+from master.operations import BlenderOperation
+from daemons import WorkerDaemon
+
+njp = BlenderOperation(1, data_packet={
+    'job_type': JobType.OPERATION,
+    'blender_file_path': '/home/batkroes/8k 120fps.blend',
+    'start_frame': 1, 'stop_frame': 120,
+    'frame_rate': '120',
+    'output_path': "/home/batkroes/8ktest",
+    'engine': "CYCLES"})
+wd = WorkerDaemon("../config/conf.yaml")
+wd.add_scheduled_job(NewOperationPacket(packet_id=1, job_type=JobType.OPERATION,
+                                        data_packet=njp))
+wd.main()
